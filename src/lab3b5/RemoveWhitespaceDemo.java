515e2f3bbe812cb22d903763e6e1375d6fee14c8
Rames <Rames@LAPTOP-SR6VDFT4>
2026-04-19 01:42:23 +0530

    lab3b5

---
+++ src/lab3b5/RemoveWhitespaceDemo.java
@@ -0,0 +1,24 @@
+package lab3b5;
+
+import java.util.Scanner;
+
+public class RemoveWhitespaceDemo {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+        String result = removeWhitespace(input);
+
+        System.out.println("String after removing whitespace: " + result);
+
+        sc.close();
+    }
+
+    // User-defined function
+    public static String removeWhitespace(String str) {
+        return str.replaceAll("\\s", "");
+    }
+}
